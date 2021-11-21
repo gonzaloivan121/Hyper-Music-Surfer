0e071784c51563609c9d2134c949165645119aa8
Gonzalo Iván Chaparro Barese <gonzaloivan121@gmail.com>
2021-11-21 16:12:23 +0100

    Agregado texto que representa la puntuacion del jugador

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameController : MonoBehaviour {
-
     public int score = 0;
-    public GameObject player;
+    public PlayerController player;
+    public TextMeshProUGUI scoreText;
+
+    public bool test;
 
     // Start is called before the first frame update
     void Start() {
-        
+        QualitySettings.vSyncCount = 1;
+        Cursor.lockState = CursorLockMode.Locked;
+        scoreText.text = "" + score;
     }
 
     // Update is called once per frame
@@ -19,5 +24,6 @@ public class GameController : MonoBehaviour {
 
     public void UpdateScore(int points) {
         score += points;
+        scoreText.text = "" + score;
     }
 }
