f94eaf0f64e3f2c596ced820b107bbb93bc5fbd0
Gonzalo Iván Chaparro Barese <gonzaloivan121@gmail.com>
2021-11-21 16:14:38 +0100

    Los objetos de puntuacion ahora se almacenan en un pool de objetos

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -2,18 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pickup : MonoBehaviour {
-    public int points = 25;
-    public GameController gameController;
+namespace SplineMesh {
+    public class Pickup : MonoBehaviour {
+        public int points = 25;
+        public GameController gameController;
+        public PickupController pickupController;
 
-    void OnTriggerEnter(Collider collider) {
-        if (collider.gameObject.tag == "Player") {
-            gameController.UpdateScore(points);
-            Destroy(gameObject);
+        void OnTriggerEnter(Collider collider) {
+            if (collider.gameObject.tag == "Player") {
+                gameController.UpdateScore(points);
+                pickupController.DequeuePickupFromPool(gameObject);
+            }
         }
-    }
 
-    public void SetGameController(GameController gc) {
-        gameController = gc;
+        public void SetGameController(GameController gc) {
+            gameController = gc;
+        }
+
+        public void SetPickupController(PickupController pc) {
+            pickupController = pc;
+        }
+
     }
 }
--- Assets/Scripts/PickupController.cs
+++ Assets/Scripts/PickupController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace SplineMesh {
-
     [RequireComponent(typeof(Spline))]
     public class PickupController : MonoBehaviour {
         public Transform pickupParent;
@@ -15,31 +14,78 @@ namespace SplineMesh {
         private Spline spline;
         private float rate = 0;
 
+        [System.Serializable]
+        public class Pool {
+            public string tag;
+            public GameObject prefab;
+            public int size;
+        }
+
+        private string pickupTag = "Pickup";
+
+        public List<Pool> pools;
+        public Dictionary<string, Queue<GameObject>> poolDictionary;
+        // Pool Test End
+
         void OnEnable() {
             rate = 0;
             spline = GetComponent<Spline>();
         }
 
         void Start() {
+            poolDictionary = new Dictionary<string, Queue<GameObject>>();
+            EnqueuePickupsInPool();
             GeneratePickups();
         }
 
+        public void DequeuePickupFromPool(GameObject pickup) {
+            pickup.SetActive(false);
+            poolDictionary[pickupTag].Dequeue();
+        }
+
+        void EnqueuePickupsInPool() {
+            foreach (Pool pool in pools) {
+                Queue<GameObject> pickupPool = new Queue<GameObject>();
+                for (int i = 0; i < pool.size; i++) {
+                    GameObject pickup = Instantiate(pickupPrefab, pickupParent);
+                    pickup.SetActive(false);
+                    pickupPool.Enqueue(pickup);
+                }
+
+                poolDictionary.Add(pool.tag, pickupPool);
+            }
+        }
+
+        GameObject SpawnFromPool(string tag, Vector3 position, Vector3 rotation) {
+            if (!poolDictionary.ContainsKey(tag)) {
+                Debug.LogWarning("Pool with tag " + tag + " doesn't exist.");
+                return null;
+            }
+
+            GameObject objectToSpawn = poolDictionary[tag].Dequeue();
+            objectToSpawn.SetActive(true);
+            objectToSpawn.transform.position = position;
+            objectToSpawn.transform.Rotate(rotation);
+
+            poolDictionary[tag].Enqueue(objectToSpawn);
+
+            return objectToSpawn;
+        }
+
         void GeneratePickups() {
-            for (float i = 0f; i < maxPickups; i += 1f) {
+            for (float i = 1f; i < maxPickups; i += 1f) {
                 float mappedRate = i.Map(0f, maxPickups, .0001f, spline.nodes.Count - 1);
                 rate = mappedRate;
                 CurveSample sample = spline.GetSample(rate);
 
-                GameObject pivot = Instantiate(pickupPrefab, pickupParent);
-                pivot.transform.position = sample.location;
-
-                Pickup pickup = pivot.GetComponentInChildren<Pickup>();
-                pickup.SetGameController(gameController);
-
                 float randomAngle = Random.Range(0, 359);
                 Vector3 randomRot = new Vector3(0f, 0f, randomAngle);
 
-                pivot.transform.Rotate(randomRot);
+                GameObject pivot = SpawnFromPool("Pickup", sample.location, randomRot);
+
+                Pickup pickup = pivot.GetComponentInChildren<Pickup>();
+                pickup.SetGameController(gameController);
+                pickup.SetPickupController(this);
             }
         }
 
